43eb314a69de7c79122897429db3a1cb691af653
Raj-Vartri <rajvardhan2128@gmail.com>
2025-06-17 16:12:56 +0530

    Add files via upload

---
+++ Assigment one/task1.py
@@ -0,0 +1,9 @@
+# Get two numbers from the user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Perform basic operations
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
