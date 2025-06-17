de89dab5cf830bb16605032da8a33f36698dc503
Raj-Vartri <rajvardhan2128@gmail.com>
2025-06-17 16:54:17 +0530

    Add files via upload

---
+++ Assignment2/task1.py
@@ -0,0 +1,9 @@
+#Takimg integer input from user
+num1=int(input("Enter a number:\t"));
+
+# Finding either number is even or odd by dividing by 2
+if(num1 %2==0):
+    print(num1,"is an even number.")
+else:
+    print(num1," is an odd number.")
+
---
+++ Assignment2/task2.py
@@ -0,0 +1,5 @@
+num=0;
+for i in range(1,51):
+    num=num+i
+print(num)
+
