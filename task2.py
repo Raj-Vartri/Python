1712d0835e624ded28120b4071f06d025f538ca7
Raj-Vartri <rajvardhan2128@gmail.com>
2025-06-17 16:22:47 +0530

    Delete task2.py

--- task2.py
+++
@@ -1,7 +0,0 @@
-#Personalized Greeting
-
-# Get user's name
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-full_name = first_name +" "+ last_name
-print("Hello,", full_name + "! Welcome to python program.")
