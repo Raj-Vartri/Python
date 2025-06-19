4c9b5c0a6e296b98e977bd56af57214db42f83c8
Raj-Vartri <rajvardhan2128@gmail.com>
2025-06-19 17:53:38 +0530

    Update task1.py

--- Assignment3/task1.py
+++ Assignment3/task1.py
@@ -1,8 +1,11 @@
 def factorial(number):
-    num = 1
-    for i in range(1,number+1):
-        num=num*i
-    return num
+    if (number<2):
+        return 1
+    else:
+        num = 1
+        for i in range(1,number+1):
+            num=num*i
+        return num
 
 user_input=int(input("Enter a number: "))
 print(f"Factorial of {user_input} is: {factorial(user_input)}")
